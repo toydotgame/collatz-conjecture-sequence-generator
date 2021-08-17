ece24ca3def29b720faf2e6aff5153ad87d4f77e
toydotgame <toydotgame@gmail.com>
2021-08-18 08:47:19 +1000

    Stabilised code; Added two config options

--- src/Main.java
+++ src/Main.java
@@ -4,49 +4,87 @@ public class Main {
 	public static long input;
 	public static long current;
 	public static long next;
-	public static long steps = 0;
-	public static Scanner inputScanner = new Scanner(System.in);
+	public static int steps = 0; // Using an int should be fine. I think the longest sequence is only a 5-digit number.
+	public static int oddTotal;
+	public static int evenTotal;
+	public static Scanner scanner = new Scanner(System.in);
+	
 	public static boolean useEquations;
+	public static boolean oddEvenDisplay;
 	
 	public static void main(String[] args) {
 		useEquations = configEquationOutput();
-		inputScanner.nextLine(); // Effectively "clears" the Scanner buffer, which may have other strings left in it (due to using `.next()`).
-
-		System.out.print("Enter an integer (preferably positive) that is less than 2^63: ");
-		input = inputScanner.nextLong();
+		scanner.nextLine(); // "Clears" Scanner buffer if anything else was entered.
 		
+		oddEvenDisplay = configOddEven();
+		scanner.nextLine();
+
+		System.out.print("Enter an integer (that is less than 2^63): ");
+		input = scanner.nextLong();
+		
+		scanner.close();
 		collatz(input);
-		inputScanner.close();
+		System.out.println("\nSteps:        " + String.valueOf(steps));
+		System.out.println("Even numbers: " + String.valueOf(evenTotal));
+		System.out.println("Odd numbers:  " + String.valueOf(oddTotal));
 	}
 
 	public static void collatz(long input) {
 		current = input;
-		System.out.println("Starting at " + String.valueOf(current));
+		System.out.println("\nStarting at " + String.valueOf(current));
 		
-		while(current > 4) {
+		while(current > 1) {
 			if(current % 2 == 0) {
 				next = current / 2;
+				evenTotal++;
 			} else {
 				next = 3 * current + 1;
+				oddTotal++;
 			}
-			System.out.println(String.valueOf(next));
+			printStep();
 			current = next;
 			steps++;
 		}
-		System.out.print("Steps: " + String.valueOf(steps));
+	}
+	
+	public static void printStep() {
+		String oddEven = "";
+		if(oddEvenDisplay) {
+			if(current % 2 == 0) {
+				oddEven = "Number was even; ";
+			} else {
+				oddEven = "Number was odd;  ";
+			}
+		}
+		
+		if(useEquations) {
+			if(current % 2 != 0) {
+				System.out.println(oddEven + "3(" + String.valueOf(current) + ") + 1 = " + String.valueOf(next));
+			} else {
+				System.out.println(oddEven + String.valueOf(current) + " / 2 = " + String.valueOf(next));
+			}
+		} else {
+			System.out.println(oddEven + String.valueOf(next));
+		}
 	}
 	
 	public static boolean configEquationOutput() {
-		while(useEquations == null) {
-		System.out.print("Print equations? [y/N] ");
-		String equationChoiceString = inputScanner.next().toLowerCase();
-		if(equationChoiceString.equals("y")) {
+			System.out.print("Print equations? [y/n] ");
+			String optionInput = scanner.next().toLowerCase();
+			if(optionInput.equals("y")) {
+				return true;
+			} else {
+				return false;
+			}
+	}
+	
+	public static boolean configOddEven() {
+		System.out.print("Say if a number is odd or even? [y/n] ");
+		String optionInput = scanner.next().toLowerCase();
+		if(optionInput.equals("y")) {
 			return true;
-		} else if(equationChoiceString.equals("n") || equationChoiceString.equals("")) {
-			return false;
 		} else {
-			
-		}
+			return false;
 		}
 	}
 }
